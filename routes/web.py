adb57942896dce2242069605f3a8be5fdd667a72
lavrentev18 <irbis_92@mail.ru>
2022-05-01 12:46:22 +0300

    Добавлен аналогичный метод для POST запроса

--- routes/web.py
+++ routes/web.py
@@ -1,8 +1,12 @@
 def register(router):
-    def index_action(context):
+    def index_action_get(context):
         context.send_response(200)
         context.end_headers()
-        context.wfile.write(b'Hello, world!')
+        context.wfile.write(b'Hello, get!')
+    def index_action_post(context):
+        context.send_response(200)
+        context.end_headers()
+        context.wfile.write(b'Hello, post!')
 
-
-    router.get("/", index_action)
+    router.get("/", index_action_get)
+    router.post("/", index_action_post)
