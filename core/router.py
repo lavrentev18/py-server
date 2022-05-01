1509d98d1917d8583cd8730bb9e3cb38cd47504f
lavrentev18 <irbis_92@mail.ru>
2022-05-01 12:47:15 +0300

    1. Изменен тип routes 2. Добавлен метод для POST запроса 3. Теперь есть проверка на тип запроса (POST or GET)

--- core/router.py
+++ core/router.py
@@ -1,14 +1,21 @@
 from routes.web import register as register_web
 
+
 class Router:
     def __init__(self):
-        self.routes = {}
+        self.routes = []
 
     def get(self, route, action):
-        self.routes[route] = action
+        self.routes.append((route, "GET", action))
+
+    def post(self, route, action):
+        self.routes.append((route, "POST", action))
 
     def handle(self, context):
-        self.routes.get(context.path)(context)
+        for i in range(len(self.routes)):
+            route_element = self.routes[i]
+            if route_element[0] == context.path and route_element[1] == context.method:
+                route_element[2](context)
 
 
 router = Router()
