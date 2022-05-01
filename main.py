1194973535ca7ffcbb7363cabc18595cb8ab38a0
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-01 20:37:52 +0300

    print до serve_forever, иначе не работает

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import config
 from core import app, DB
 import os
 
 def run():
     try:
         DB.connect()
+        print(f"Server started in http://{config.HOST}:{config.PORT}", flush = True)
         app.serve_forever()
-        print("Server in started in http://" + os.getenv("HOST") + ":" + (os.getenv("PORT")), flush=True)
     except Exception as e:
         print(e)
         DB.disconnect()
