4d9860c011acfd908eb25de9b3a59db11e004800
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-01 20:18:05 +0300

    лог о старте сервера после старта сервера

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from core import app, DB
 import os
 
-
 def run():
     try:
-        print("Server in started in http://" + os.getenv("HOST") + ":" + (os.getenv("PORT")), flush=True)
         DB.connect()
         app.serve_forever()
+        print("Server in started in http://" + os.getenv("HOST") + ":" + (os.getenv("PORT")), flush=True)
     except Exception as e:
         print(e)
         DB.disconnect()
         app.shutdown()
-
-
 
 run()
 
