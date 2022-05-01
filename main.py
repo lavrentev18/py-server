147a876ac26801545988c3bed95b5784a8ebe3fd
lavrentev18 <irbis_92@mail.ru>
2022-05-01 13:37:01 +0300

    Исправлен вывод в терминал

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 def run():
     try:
-        print("Server in started in http://" + os.getenv("HOST") + ":" + (os.getenv("PORT")))
+        print("Server in started in http://" + os.getenv("HOST") + ":" + (os.getenv("PORT")), flush=True)
         app.serve_forever()
     except Exception as e:
         print(e)
