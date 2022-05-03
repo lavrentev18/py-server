5b84709f20d8347ece8f8d56d2d05ed54b2c9ba7
lavrentev18 <irbis_92@mail.ru>
2022-05-03 14:46:06 +0300

    added flush

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def run():
         print(f"Server started in http://{config.HOST}:{config.PORT}", flush = True)
         app.serve_forever()
     except Exception as e:
-        print(e)
+        print(e, flush=True)
         DB.disconnect()
         app.shutdown()
 
