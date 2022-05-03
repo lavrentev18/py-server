b85a4bf6e78499bf39d50ffc5f3b6425f7b1bb03
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-03 19:26:23 +0300

    сервер перемещен в нужный модуль

--- core/http/server.py
+++ core/http/server.py
@@ -1,34 +1,23 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from core.router import router
-from core.view import templateEngine
+from .request import Request
+from .response import Response
 import config
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server)
-        self.method = ""
-
-    def render(self, *args):
-        result = templateEngine.render(*args)
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(bytes(result, "utf8"))
-
-    def handleError(self, code = 500):
-        result = templateEngine.render(str(code))
-
-        self.send_response(code)
-        self.end_headers()
-        self.wfile.write(bytes(result, "utf8"))
-
     def do_GET(self):
-        self.method = "GET"
-        router.handle(self)
+        self.__createRequestAndResponse()
+        self.req.method = "GET"
+        return router.handle(self.req, self.res)
 
     def do_POST(self):
-        self.method = "POST"
-        router.handle(self)
+        self.__createRequestAndResponse()
+        self.req.method = "POST"
+        return router.handle(self.req, self.res)
+
+    def __createRequestAndResponse(self):
+        self.req = Request(self)
+        self.res = Response(self)
 
 
 app = HTTPServer((config.HOST, int(config.PORT)), SimpleHTTPRequestHandler)
