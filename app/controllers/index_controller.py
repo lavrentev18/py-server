14f7cb84f05c8c8444a37313ecd5fa6c143a763a
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-03 19:15:06 +0300

    экшен контроллера + маршрут для показа единичной записи

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -2,13 +2,26 @@ from core.database import DB
 
 class IndexController:
     @staticmethod
-    def index(context):
+    def index(req, res):
         try:
             Posts = DB.execute("SELECT * FROM posts").toDict(("id", "title", "description"))
-            context.render("index", {'posts': Posts})
+            
+            return res.render("index", { 'posts': Posts })
         except Exception as e:
             print(e, flush = True)
-            context.handleError()
+            res.handleError()
+
+    @staticmethod
+    def show(req, res):
+        try:
+            id = req.params.get("id")
+
+            [Post] = DB.execute(f"SELECT * FROM posts WHERE id = {id}").toDict(("id", "title", "description"))
+
+            return res.render("post", { 'post': Post })
+        except Exception as e:
+            print(e, flush = True)
+            return res.handleError()
 
     @staticmethod
     def create(context):
--- routes/web.py
+++ routes/web.py
@@ -3,6 +3,7 @@ from app.controllers.index_controller import IndexController
 
 def register(router):
     router.get("/", IndexController.index)
+    router.get("/:id", IndexController.show)
     router.post("/", IndexController.create)
 
 
